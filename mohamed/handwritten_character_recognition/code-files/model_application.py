9c5a82e93588c75dd3fddda1ea957e1408842276
Mohamed NIANG <mouhamedniang1997@gmail.com>
2023-07-13 18:10:58 +0000

    added a new model for handwritten character recognition

---
+++ mohamed/handwritten_character_recognition/code-files/model_application.py
@@ -0,0 +1,47 @@
+from keras.datasets import mnist
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from keras.optimizers import SGD, Adam
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.utils import to_categorical
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+from tqdm import tqdm_notebook
+from sklearn.utils import shuffle
+
+model = load_model(r'C:\model_hand.h5')
+
+# Dictionary for getting characters from index values...
+word_dict = {0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G',7:'H',8:'I',9:'J',10:'K',11:'L',12:'M',13:'N',14:'O',15:'P',16:'Q',17:'R',18:'S',19:'T',20:'U',21:'V',22:'W',23:'X', 24:'Y',25:'Z'}
+
+
+img = cv2.imread(r'C:\M.png')
+img_copy = img.copy()
+
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = cv2.resize(img, (400,440))
+
+img_copy = cv2.GaussianBlur(img_copy, (7,7), 0)
+img_gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
+_, img_thresh = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY_INV)
+
+img_final = cv2.resize(img_thresh, (28,28))
+img_final =np.reshape(img_final, (1,28,28,1))
+
+
+img_pred = word_dict[np.argmax(model.predict(img_final))]
+
+cv2.putText(img, "Prediction: " + img_pred, (20,410), cv2.FONT_HERSHEY_DUPLEX, 1.3, color = (255,0,30))
+cv2.imshow('Dataflair handwritten character recognition _ _ _ ', img)
+
+while (1):
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+cv2.destroyAllWindows()
